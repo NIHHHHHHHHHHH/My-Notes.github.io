01cff092fabc4e291672b31734e0f7af73ce3fb5
NIHHHHHHHHHHH <107672256+NIHHHHHHHHHHH@users.noreply.github.com>
2023-02-07 11:06:16 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Highlight } from '@material-ui/icons';
+
+function Header() {
+    return (
+        <header>
+            <div>
+                <h1>
+                    <Highlight />
+                    MY NOTES</h1>
+            </div>
+        </header>
+    )
+
+}
+
+export default Header;
